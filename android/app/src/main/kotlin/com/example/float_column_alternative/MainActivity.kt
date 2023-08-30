265d71895b1c4c7a3134cd0cf1e0afdfe8cdc38a
Rohan.Solse <rohan.solse@baptisthealth.net>
2023-08-30 13:40:30 +0530

    float_column done

---
+++ android/app/src/main/kotlin/com/example/float_column_alternative/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.float_column_alternative
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+class TextConstants {
+  static const String text =
+      'This is text which will expand soon. Flutter FloatColumn widget for building a vertical column of widgets and text where the text wraps around floated widgets, similar to how CSS float works. 2. This is text which will expand soon. Flutter FloatColumn widget for building a vertical column of widgets and text where the text wraps around floated widgets, similar to how CSS float works';
+}
