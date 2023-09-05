8f93b6721cf447f60510c91c564d5c9d78b6de37
Rohan.Solse <rohan.solse@baptisthealth.net>
2023-09-05 13:08:16 +0530

    changes with scaling done

--- lib/image_view.dart
+++ lib/image_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-class ImageView extends StatelessWidget {
-  const ImageView({super.key});
+class InteractiveViewerExample extends StatelessWidget {
+  const InteractiveViewerExample({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Image.network('https://picsum.photos/250?image=9'));
+    return Center(
+      child: InteractiveViewer(
+        boundaryMargin: const EdgeInsets.all(10.0),
+        minScale: 0.1,
+        maxScale: 1.6,
+        child: Image.network('https://picsum.photos/250?image=9'),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const ImageView(),
+      body: const InteractiveViewerExample(),
     );
   }
 }
