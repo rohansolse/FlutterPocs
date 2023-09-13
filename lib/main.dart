e015438575bf48b639237d9a9d6a64b3b6e9c4db
Rohan.Solse <rohan.solse@baptisthealth.net>
2023-09-13 18:03:58 +0530

    singel time zoom done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:float_column_alternative/image_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,14 +29,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late TransformationController controller;
+  TapDownDetails? tapDownDetails;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TransformationController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Zoomable Image'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GestureDetector(
+            onDoubleTapDown: (details) => tapDownDetails = details,
+            onDoubleTap: () {
+              final position = tapDownDetails!.localPosition;
+              const double scale = 1.5;
+              final x = -position.dx * (scale - 1);
+              final y = -position.dy * (scale - 1);
+              final zoomed = Matrix4.identity()
+                ..translate(x, y)
+                ..scale(scale);
+              final value = controller.value.isIdentity() ? zoomed : Matrix4.identity();
+              controller.value = value;
+            },
+            child: InteractiveViewer(
+              clipBehavior: Clip.none,
+              transformationController: controller,
+              panEnabled: false,
+              scaleEnabled: true,
+              child: Image.asset('assets/images/01.jpg'),
+            ),
+          ),
+        ),
       ),
-      body: const InteractiveViewerExample(),
     );
   }
 }
