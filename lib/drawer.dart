cf9a8c7d7e44e8e58ab7137117704dbc1a99257b
Rohan.Solse <rohan.solse@baptisthealth.net>
2023-09-18 14:27:12 +0530

    working

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -7,33 +7,42 @@ class DrawerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
+      child: Column(
         children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: MyColors.primaryColor,
+          const SizedBox(
+            width: double.infinity,
+            child: DrawerHeader(
+              decoration: BoxDecoration(
+                color: MyColors.primaryColor,
+              ),
+              child: Text('Drawer Header'),
             ),
-            child: Text('Drawer Header'),
           ),
-          ListTile(
-            leading: const Icon(
-              Icons.home,
+          Expanded(
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: [
+                ListTile(
+                  leading: const Icon(
+                    Icons.home,
+                  ),
+                  title: const Text('Page 1'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.train,
+                  ),
+                  title: const Text('Page 2'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
             ),
-            title: const Text('Page 1'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.train,
-            ),
-            title: const Text('Page 2'),
-            onTap: () {
-              Navigator.pop(context);
-            },
           ),
         ],
       ),
